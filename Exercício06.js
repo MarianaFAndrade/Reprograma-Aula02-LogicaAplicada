3f7d252444d8187a34e4cfa6fcb0d13a0e7650f3
Mariana Andrade <andrade.marianaferreira@gmail.com>
2021-05-19 20:57:18 -0300

    Ajuste no exercício 06

--- Exercício06.js
+++ Exercício06.js
@@ -2,7 +2,7 @@
 
 //Opção1
 
-let diasDaSemana = 5
+let diasDaSemana =11
 
 if (diasDaSemana === 1){
     console.log ('Domingo')
@@ -22,34 +22,38 @@ else if (diasDaSemana === 5){
 else if (diasDaSemana === 6){
     console.log ('Sexta')
 }
-else {
+else if (diasDaSemana === 7){
     console.log ('Sábado')    
+}
+else {
+    console.log ('Digite uma entrada válida')
 }
 // Opção
 
-let diaDaSemana = '6'
+let diaDaSemana = 6
 
 switch (diaDaSemana) {
-    case '1':
+    case 1:
         console.log ('Domingo')
         break
-    case '2':
+    case 2:
         console.log ('Segunda')
         break
-    case '3':
+    case 3:
         console.log ('Terça')
         break
-    case '4':
+    case 4:
         console.log ('Quarta')
         break
-    case '5':
+    case 5:
         console.log ('Quinta')
         break
-    case '6':
+    case 6:
         console.log ('Sexta')
         break
-    case '7':
+    case 7:
         console.log ('Sábado')
         break
     default:
+        console.log ('Digite uma entrada válida')
 }
