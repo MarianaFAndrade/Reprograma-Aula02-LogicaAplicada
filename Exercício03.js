bce38cb520dadcb56e96e5ff70f949a1b859986c
Mariana Andrade <andrade.marianaferreira@gmail.com>
2021-05-19 21:27:31 -0300

    Corrigindo o exercício 03

--- Exercício03.js
+++ Exercício03.js
@@ -1,8 +1,7 @@
 // 3 - Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
 
 let valorJantar = 500
-const taxaDoGarçom = valorJantar *(10/100)
-    console.log (taxaDoGarçom)
 
-const valorDaConta = valorJantar - taxaDoGarçom
-    console.log (valorDaConta)
+const taxaDoGarçom = valorJantar *(10/100)
+const valorDaConta = valorJantar + taxaDoGarçom
+    console.log (`A taxa do garçom é R$ ${taxaDoGarçom} | O valor total da conta é R$ ${valorDaConta}`)
